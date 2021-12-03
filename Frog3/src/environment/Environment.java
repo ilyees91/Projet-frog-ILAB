3c1c0a108b414de25f840b709fcba9e42c81816d
ilyees91 <93188666+ilyees91@users.noreply.github.com>
2021-12-03 08:05:02 +0100

    Add files via upload

---
+++ Frog3/src/environment/Environment.java
@@ -0,0 +1,51 @@
+package environment;
+
+import java.util.ArrayList;
+
+import util.Case;
+import gameCommons.Game;
+import gameCommons.IEnvironment;
+
+public class Environment implements IEnvironment {
+    private Game game;
+    private ArrayList<Lane> ensLane;
+
+    public Environment(Game game) {
+        this.game = game;
+        this.ensLane = new ArrayList<Lane>();
+        for(int i=0; i<game.height;i++){
+            this.ensLane.add(new Lane (game, i,i%2!=0));
+        }
+
+    }
+    public boolean isSafe(Case c){
+        Lane atm = ensLane.get(c.ord);
+        return atm.isSafe(c);
+    }
+
+    @Override
+    public boolean isWinningPosition(Case c) {
+        if (c.ord==this.game.height-1){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void update() {
+        for (int i=1; i< ensLane.size()-1; i++){
+           ensLane.get(i).update();
+        }
+    }
+    public void moveLane() {
+        for (int i=1; i< ensLane.size()-1; i++){
+            ensLane.get(i).update();
+        }
+    }
+    public void addLane(){
+        this.ensLane.add(new Lane (game, this.ensLane.size(),this.ensLane.size()%2!=0));
+    }
+    public int getSize(){
+        return this.ensLane.size();
+    }
+}
